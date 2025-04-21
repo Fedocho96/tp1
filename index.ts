b647c3dd217426f3490a1afa10eecef7c9fddc32
Federico Rosales <fedocho.96@gmail.com>
2025-04-21 14:14:22 -0300

    Interface and userschema
    interface and userschema done

--- index.ts
+++ index.ts
@@ -1,3 +1,30 @@
-import {connectDB} from "./config/mongo";
+import mongoose, { Document, Schema } from "mongoose";
 
-connectDB()
+import { connectDB } from "./config/mongo";
+connectDB();
+
+interface User extends Document {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  role: "user" | "admin";
+}
+
+const userSchema: Schema = new Schema<User>(
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  {
+    versionKey: false,
+  }
+);
