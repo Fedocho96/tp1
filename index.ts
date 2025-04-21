23b638f2cb318c95ccd3d12e024fed25da4aa0f1
Federico Rosales <fedocho.96@gmail.com>
2025-04-21 14:32:35 -0300

    create and find done
    create a hardcoded user and find all users functions done

--- index.ts
+++ index.ts
@@ -3,15 +3,15 @@ import mongoose, { Document, Schema } from "mongoose";
 import { connectDB } from "./config/mongo";
 connectDB();
 
-interface User extends Document {
+interface UserInterface extends Document {
   name: string;
   age: number;
   email: string;
   password: string;
-  role: "user" | "admin";
+  role?: "user" | "admin";
 }
 
-const userSchema: Schema = new Schema<User>(
+const userSchema: Schema = new Schema<UserInterface>(
   {
     name: { type: String, required: true },
     age: { type: Number, required: true },
@@ -28,3 +28,32 @@ const userSchema: Schema = new Schema<User>(
     versionKey: false,
   }
 );
+userSchema.set("strict", true);
+
+const User = mongoose.model<UserInterface>("user", userSchema);
+
+const createUser = async () => {
+  try {
+    const user: UserInterface = new User({
+      name: "Jorge Test",
+      age: 30,
+      email: "Jorgetest@gmail.com",
+      password: "123456",
+      role: "user",
+    });
+    await user.save();
+    console.log("User created:", user);
+  } catch (error) {
+    console.error("Error creating user:" + error);
+  }
+};
+
+const getUsers = async () => {
+    try {
+        const users = await User.find();
+        console.log("Users found:", users);
+    } catch (error) {
+        console.error("Error getting users:" + error);
+    }
+    }
+    
